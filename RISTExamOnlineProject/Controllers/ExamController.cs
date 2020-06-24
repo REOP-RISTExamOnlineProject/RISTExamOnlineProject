c23a14503c4120b68bf89402e23ca5172da7a718
WiNo57346 <winozuza01@gmail.com>
2020-06-24 09:10:17 +0700

    Exam

---
+++ RISTExamOnlineProject/Controllers/ExamController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
+using RISTExamOnlineProject.Models.db;
+using RISTExamOnlineProject.Models.TSQL;
+
+namespace RISTExamOnlineProject.Controllers
+{
+    public class ExamController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly SPTODbContext _sptoDbContext;
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+
+        public ExamController(SPTODbContext context, IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
+        {
+
+            _sptoDbContext = context;
+            _configuration = configuration;
+            this.httpContextAccessor = httpContextAccessor;
+
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Examination_Modal()
+        {
+
+            return View();
+
+
+        }
+
+        public IActionResult Examination()
+        { 
+        
+        return View();
+        
+        
+        }
+
+
+        public IActionResult GetHTML(string ItemCateg, string ItemCode) {
+
+
+            mgrSQLcommand_Exam ObjRun = new mgrSQLcommand_Exam(_configuration);
+
+            string HTMLTEXT = ObjRun.GetExamHTML(ItemCateg, ItemCode);
+
+            return Json(new { success = true , HTMLTEXT = HTMLTEXT });
+
+        }
+
+        public IActionResult GetCategory()
+        {
+
+            mgrSQLcommand_Exam ObjRun = new mgrSQLcommand_Exam(_configuration);
+            List<SelectListItem> listItems = new List<SelectListItem>();
+            listItems = ObjRun.GetCategory();
+            return Json(new MultiSelectList(listItems, "Value", "Text"));
+
+        }
+        
+
+        public IActionResult GetExamname(string Category)
+        {
+
+            mgrSQLcommand_Exam ObjRun = new mgrSQLcommand_Exam(_configuration);
+            List<SelectListItem> listItems = new List<SelectListItem>();
+            listItems = ObjRun.GetExamName(Category);
+            return Json(new MultiSelectList(listItems, "Value", "Text"));
+
+
+        }
+        
+
+
+    }
+}
---
+++ RISTExamOnlineProject/Models/TSQL/mgrSQLcommand_Exam.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RISTExamOnlineProject.Models.TSQL
+{
+    public class mgrSQLcommand_Exam
+    {
+        private readonly IConfiguration _configuration;
+        private DataSet ds = new DataSet();
+        private DataTable dt = new DataTable();
+        private string strSQL = "";
+        public mgrSQLcommand_Exam(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetExamHTML(string ItemCateg, string ItemCode)
+        {
+
+            string HTMLTEXT = "";
+
+            var ObjRun = new mgrSQLConnect(_configuration);
+            strSQL = " [dbo].[srpMakeHTMLQuestion]	'"+ ItemCateg + "', '"+ ItemCode + " '" ;
+
+            dt = ObjRun.GetDatatables(strSQL);
+
+            if (dt.Rows.Count > 0) 
+            {
+                HTMLTEXT = dt.Rows[0][0].ToString(); 
+
+            }         
+
+
+            return HTMLTEXT;
+        }
+
+
+        public List<SelectListItem> GetCategory() {
+
+            var ObjRun = new mgrSQLConnect(_configuration);
+            strSQL = "  SELECT[ItemCateg],[ItemCategName]  FROM[SPTOSystem].[dbo].[vewQuestionNull] group by[ItemCateg],[ItemCategName]";
+
+            dt = ObjRun.GetDatatables(strSQL);
+            List<SelectListItem> listItems = new List<SelectListItem>();
+
+            if (dt.Rows.Count != 0)
+            {
+                listItems.Add(new SelectListItem()
+                {
+                    Text = "--- Choose Category ---",
+                    Value = ""
+                });
+                foreach (DataRow row in dt.Rows)
+                {
+                    listItems.Add(new SelectListItem()
+                    {
+                        Text = row["ItemCateg"].ToString().Trim() + "-" +row["ItemCategName"].ToString().Trim(),
+                        Value = row["ItemCateg"].ToString().Trim(),
+
+                    });
+                }
+            }
+
+            return listItems;
+
+        }
+
+        public List<SelectListItem> GetExamName(string ItemCateg)
+        {
+
+            var ObjRun = new mgrSQLConnect(_configuration);
+            strSQL = "SELECT    [ItemCode],[ItemName]   FROM [SPTOSystem].[dbo].[vewQuestionNull] where[ItemCateg] = '"+ ItemCateg + "' group by[ItemName],[ItemCode]";
+
+            dt = ObjRun.GetDatatables(strSQL);
+            List<SelectListItem> listItems = new List<SelectListItem>();
+
+            if (dt.Rows.Count != 0)
+            {
+                listItems.Add(new SelectListItem()
+                {
+                    Text = "--- Choose Exam name ---",
+                    Value = ""
+                });
+                foreach (DataRow row in dt.Rows)
+                {
+                    listItems.Add(new SelectListItem()
+                    {
+                        Text = row["ItemCode"].ToString().Trim() +"-"+row["ItemName"].ToString().Trim(),
+                        Value = row["ItemCode"].ToString().Trim(),
+
+                    });
+                }
+            }
+
+            return listItems;
+
+        }
+
+
+    }
+}
