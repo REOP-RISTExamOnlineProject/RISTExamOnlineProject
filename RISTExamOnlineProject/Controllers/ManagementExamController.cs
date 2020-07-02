165717a548ee8427547df2ad1dc40d7d78d248f4
NGAGEZA <44493178+NGAGEZA@users.noreply.github.com>
2020-07-02 18:34:02 +0700

    last day

---
+++ RISTExamOnlineProject/Controllers/ManagementExamController.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RISTExamOnlineProject.Models.db;
+
+namespace RISTExamOnlineProject.Controllers
+{
+    public class ManagementExamController : Controller
+    {
+        private readonly SPTODbContext _sptoDbContext;
+
+        public ManagementExamController(SPTODbContext context)
+        {
+            _sptoDbContext = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _sptoDbContext.ItemCategory.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public async Task<IActionResult> Create_Post(ItemCategory itemCategory)
+        {
+            if (!ModelState.IsValid) return View();
+            await _sptoDbContext.ItemCategory.AddAsync(itemCategory);
+            await _sptoDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+
+        }
+
+        public async Task<IActionResult> Update(string id)
+        {
+            return View(await _sptoDbContext.ItemCategory.FirstOrDefaultAsync(a => a.ItemCateg == id));
+        }
+
+        [HttpPost]
+        [ActionName("Update")]
+        public async Task<IActionResult> Update_Post(ItemCategory itemCategory)
+        {
+            _sptoDbContext.ItemCategory.Update(itemCategory);
+            await _sptoDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        //[HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var queryItemCategory = await _sptoDbContext.ItemCategory.FirstOrDefaultAsync(x => x.ItemCateg == id);
+            _sptoDbContext.ItemCategory.Remove(queryItemCategory);
+            await _sptoDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ RISTExamOnlineProject/Models/db/ManagementExam.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RISTExamOnlineProject.Models.db
+{
+    public class ItemCategory
+    {
+        [Key]
+        [Required]
+        [DisplayName("ItemCategKey")]
+        public string ItemCateg { get; set; }
+        [Required]
+        public string ItemCategName { get; set; }
+        [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        //[DatabaseGenerated(DatabaseGeneratedOption.)]
+        public DateTime AddDate { get; set; }
+        // [Required]
+        [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdDate { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string ComputerName { get; set; }
+       
+    }
+
+    public class InputItemList
+    {
+        public string ItemCateg { get; set; }
+        public string ItemCode{ get; set; }
+        public string ItemName { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime AddDate { get; set; }
+        public DateTime UpdDate { get; set; }
+        public string UserName { get; set; }
+        public string ComputerName { get; set; }
+    }
+
+}
